c610ac12a895e64a0d2adb4760170d80e4377ed4
Linda Ngunjiri <ciiru.ngunjiri@gmail.com>
2025-08-10 00:41:48 +0300

    Add Docker support with Dockerfile and Compose setup for MySQL

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'messagingdb'),
+        'USER': os.getenv('DB_USER', 'messaginguser'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'securepassword'),
+        'HOST': os.getenv('DB_HOST', 'db'),  # service name from docker-compose
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
 
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from chats.auth import CustomTokenObtainPairView, CustomTokenVerifyView, CustomTokenRefreshView
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, world!")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
     path('api/', include('chats.urls')),
     path('api-auth/', include('rest_framework.urls')),  # Browsable API login/logout
 ]
